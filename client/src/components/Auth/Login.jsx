3c84f298cfb67b33279875b8e1cba5e9f7d313ff
GaYeong <rkdud5483@gmail.com>
2023-03-04 23:07:25 +0900

    March04

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../api/firebase';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import AuthNav from './AuthNav';
@@ -7,6 +9,7 @@ import { BiShow } from 'react-icons/bi';
 import { BiHide } from 'react-icons/bi';
 
 export default function Login() {
+  const [user] = useAuthState(auth);
   const navigate = useNavigate();
   const [open, setOpen] = useState(false);
   const toggle = () => {
@@ -24,31 +27,33 @@ export default function Login() {
             <p className='mt-2 text-lg leading-8 text-gray-600'>
               Explore thousands of experiences and make friends as a member.
             </p>
-            
-            <button
-              type='button'
-              className='mt-8 w-full justify-center text-white bg-red-700 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-semibold rounded-md text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2'
-              onClick={() => loginGoogle(
-                  navigate('/')
-              )}
-            >
-              <svg
-                className='w-4 h-4 mr-2 -ml-1'
-                aria-hidden='true'
-                focusable='false'
-                data-prefix='fab'
-                data-icon='google'
-                role='img'
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 488 512'
+
+            {user ? (
+              navigate('/')
+            ) : (
+              <button
+                type='button'
+                className='mt-8 w-full justify-center text-white bg-red-700 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-semibold rounded-md text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2'
+                onClick={() => loginGoogle()}
               >
-                <path
-                  fill='currentColor'
-                  d='M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z'
-                ></path>
-              </svg>
-              Sign up with Google
-            </button>
+                <svg
+                  className='w-4 h-4 mr-2 -ml-1'
+                  aria-hidden='true'
+                  focusable='false'
+                  data-prefix='fab'
+                  data-icon='google'
+                  role='img'
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 488 512'
+                >
+                  <path
+                    fill='currentColor'
+                    d='M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z'
+                  ></path>
+                </svg>
+                Sign up with Google
+              </button>
+            )}
             <p className='mt-5'>or</p>
           </div>
 
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -37,47 +37,63 @@ router.post('/login', async (req, res, next) => {
       return next(
         createError(403, 'Wrong password or username, please check out!')
       );
-    //Access token
-    const accessToken = jwt.sign(
-      {
-        id: user._id,
-        email: user.email,
-      },
-      process.env.ACCESS_SECRET,
-      {
-        expiresIn: '1h',
-        issuer: 'Park29',
-      }
-    );
-    //refresh token
-    const refreshToken = jwt.sign(
-      {
-        id: user._id,
-        email: user.email,
-      },
-      process.env.REFRESH_SECRET,
-      {
-        expiresIn: '24h',
-        issuer: 'Park29',
-      }
-    );
-    res.cookie('accessToken', accessToken, {
-      //About http & https
-      httpOnly: true,
-      //This means that the token cannot be accessed from javaScript
-    });
-    res
-      .cookie('refreshToken', refreshToken, {
+    if (user && isPasswordCorrect) {
+      const accessToken = jwt.sign(
+        {
+          id: user._id,
+          email: user.email,
+        },
+        process.env.ACCESS_SECRET,
+        {
+          expiresIn: '1h',
+          issuer: 'Park29',
+        }
+      );
+      const refreshToken = jwt.sign(
+        {
+          id: user._id,
+          email: user.email,
+        },
+        process.env.REFRESH_SECRET,
+        {
+          expiresIn: '24h',
+          issuer: 'Park29',
+        }
+      );
+      res.cookie('accessToken', accessToken, {
         //About http & https
         httpOnly: true,
-        //It means that the token cannot be accessed from javaScript
-      })
-      .status(200)
-      .json(user);
+        //This means that the token cannot be accessed from javaScript
+      });
+      res
+        .cookie('refreshToken', refreshToken, {
+          //About http & https
+          httpOnly: true,
+          //It means that the token cannot be accessed from javaScript
+        })
+        .status(200)
+        .send('Login success!');
+    }
   } catch (error) {
     next(error);
   }
 });
+
+router.get('accesstoken', async (req,res,next) => {
+  try{
+    const accessToken = req.cookies.accessToken;
+    const data = jwt.verify(accessToken, process.env.ACCESS_SECRET);
+
+    const userData = User.filter(item => {
+      return item.email === data.email;
+    })
+    res.status(200).json(userData)
+    
+  } catch(error) {
+    res.status(500).json(error)
+  }
+
+})
 
 // app.get("/login/success", loginSuccess);
 // app.post("/logout", logout);
